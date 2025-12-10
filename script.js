fcf0f2a2fc9d2ed2d449e17b0736e2860f172043
Dorcas Okeleji <okelejidorcas@gmail.com>
2025-12-10 09:02:07 +0100

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-form.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 const form = document.getElementById("registration-form");
 const feedbackDiv = document.getElementById("form-feedback");
 
@@ -8,11 +8,10 @@ form.addEventListener("submit", function(event) {
 
     //Initialize validation variable
     let isValid = true;
-    let message = [];
+    let messages = [];
     
     //validation for username
-const usernameInput = document.getElementById("Username");
-const usernameError = document.getElementById("usernameError");
+const usernameInput = document.getElementById("username");
 if (usernameInput.value.trim().length <3) {
     isValid = false;
     messages.push("username must be at least three characters long.");
@@ -20,7 +19,6 @@ if (usernameInput.value.trim().length <3) {
 
 //validation for email
 const emailInput = document.getElementById("email");
-const emailError = document.getElementById("emailError");
 if (!emailInput.value.includes('@') || !emailInput.value.includes('.')) {
     isValid = false;
     messages.push("Email must contain both '@' and '.' characters.");
@@ -28,7 +26,6 @@ if (!emailInput.value.includes('@') || !emailInput.value.includes('.')) {
 
 //validation for password
 const passwordInput = document.getElementById("password");
-const passwordError = document.getElementById("passwordError");
 if (passwordInput.value.length <8) {
     isValid = false;
     messages.push("password must contain at least 8 length");
@@ -38,13 +35,10 @@ if (passwordInput.value.length <8) {
 feedbackDiv.style.display = "block";
 
 if (isValid) {
-    feedbackDiv.textContent = "Registration Successful!";
+    feedbackDiv.textContent = "Registration successful!";
     feedbackDiv.style.color = "#28a745";
 } else {
     feedbackDiv.innerHTML = messages.join("<br>");
     feedbackDiv.style.color = "#dc3545";
-
-//if all validation pass subit the form
-form.onsubmit;
 }
-})
+});
